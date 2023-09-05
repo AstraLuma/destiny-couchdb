ce2cbdae84ce133c3bfe55adb7128a2c453f2888
Jamie Bliss <jamie@ivyleav.es>
2023-09-05 01:22:18 -0400

    add support for sqlite, gear/dyes

--- destiny_couchdb/__init__.py
+++ destiny_couchdb/__init__.py
@@ -1,12 +1,13 @@
-import asyncio
+import json
 import functools
+import itertools
 import os
 import re
-import sys
 
 import httpx
+import aioitertools
 
-from .client import Bungie
+from .client import Bungie, path
 from .couchdb import CouchDB
 from .manifest import get_manifest
 
@@ -28,18 +29,17 @@ def dbname(txt):
     return txt
 
 
-async def sync_databases(couchdb, world):
+async def sync_databases(couchdb, world, more=[]):
     existing_dbs = set((await couchdb.get('/_all_dbs')).json())
-    new_dbs = set(dbname(k) for k in world.keys())
+    new_dbs = set(dbname(k)
+                  for k in itertools.chain(world.keys(), ('meta',), more))
     for db in (new_dbs - existing_dbs):
         await couchdb.put(f'/{db}')
 
 
 # FIXME: Use bulk get/update APIs
 
-async def sync_entity(couchdb, kind, entity):
-    url = f"/{kind}/{entity['hash']}"
-    print(url)
+async def patch_document(couchdb, url, data):
     try:
         old_doc = (await couchdb.get(url)).json()
     except httpx.HTTPStatusError as exc:
@@ -48,10 +48,16 @@ async def sync_entity(couchdb, kind, entity):
         else:
             raise
 
-    new_doc = old_doc | entity
+    new_doc = old_doc | data
 
     if new_doc != old_doc:
         await couchdb.put(url, params=new_doc.get('_rev', None), json=new_doc)
+
+
+async def sync_entity(couchdb, kind, entity):
+    url = f"/{kind}/{entity['hash']}"
+    print(url)
+    await patch_document(couchdb, url, entity)
 
 
 async def sync_manifest(couchdb, manifest):
@@ -66,12 +72,72 @@ async def sync_manifest(couchdb, manifest):
             await sync_entity(couchdb, dbname(kind), item)
 
 
+async def iter_tables(sql):
+    async with sql.execute("SELECT * FROM sqlite_master WHERE type='table'") as cursor:
+        async for row in cursor:
+            yield row['name']
+
+
+async def iter_table(sql, table):
+    async with sql.execute(f"SELECT id, json FROM {table}") as cursor:
+        async for row in cursor:
+            data = json.loads(row['json'])
+            hash = int.from_bytes(row['id'].to_bytes(4, signed=True))
+            yield hash, data
+
+
+async def update_metadata(couchdb, manifest):
+    await patch_document(couchdb, '/meta/world', {
+        'version': manifest['version']
+    })
+    await patch_document(couchdb, '/meta/gear_cdn', manifest['mobileGearCDN'])
+
+
+async def update_gear(couchdb, bungie):
+    prefix = (await couchdb.get('/meta/gear_cdn')).json()['Gear']
+    all_docs = (await couchdb.get('/gear_assets/_all_docs', params={'include_docs': True})).json()
+
+    gearfiles = {
+        gfn
+        for row in all_docs['rows']
+        for gfn in row['doc']['gear']
+    }
+
+    dyes = {}
+    for gf in gearfiles:
+        hash = gf.removesuffix('.js')
+        data = (await bungie.get(path(f"{prefix}/{gf}"))).json()
+        data['hash'] = hash
+        await sync_entity(couchdb, 'gear', data)
+
+        for dye in itertools.chain(data['default_dyes'], data['locked_dyes'], data['custom_dyes']):
+            if dye['hash'] in dyes:
+                assert dye == dyes[dye['hash']
+                                   ], f"old={dyes[dye['hash']]!r} new={dye!r}"
+            dyes[dye['hash']] = dye
+
+    for dye in dyes.values():
+        await sync_entity(couchdb, 'dye_manifest', dye)
+
+
 async def main():
     async with (
             Bungie(os.environ['BUNGIE_KEY']) as bungo,
             CouchDB('http://localhost:5984', auth=('admin', 'admin')) as couchdb,
     ):
         mani = await get_manifest(bungo)
-        world = await mani.get_world_json()
-        await sync_databases(couchdb, world)
-        await sync_manifest(couchdb, world)
+        async with mani.get_gear_database() as gear:
+            # Update from the world data
+            world = await mani.get_world_json()
+            tables = await aioitertools.list(iter_tables(gear))
+            await sync_databases(couchdb, world, tables+['gear', 'dye_manifest'])
+            # await sync_manifest(couchdb, world)
+            await update_metadata(couchdb, mani._data)
+
+            # Update from the gear data
+            for table in tables:
+                async for hash, entity in iter_table(gear, table):
+                    entity['hash'] = hash
+                    # await sync_entity(couchdb, dbname(table), entity)
+
+            await update_gear(couchdb, bungo)
--- destiny_couchdb/manifest.py
+++ destiny_couchdb/manifest.py
@@ -1,3 +1,9 @@
+import contextlib
+import tempfile
+import zipfile
+
+import aiosqlite
+
 from .client import parse_response, path
 
 
@@ -15,6 +21,44 @@ class Manifest:
         fragment = self._data['jsonWorldComponentContentPaths'][lang][component]
         resp = await self._client.get(path(fragment))
         return resp.json()
+
+    @contextlib.asynccontextmanager
+    async def _get_sqlite(self, url):
+        """
+        Downloads a database from bungie, extracts the zip, and hands it to sqlite
+        """
+        with tempfile.NamedTemporaryFile('w+b') as sqlf:
+            with tempfile.TemporaryFile('w+b') as wrapperf:
+                async with self._client.stream('GET', url) as response:
+                    async for chunk in response.aiter_bytes():
+                        wrapperf.write(chunk)
+
+                wrapperf.seek(0)
+
+                with zipfile.ZipFile(wrapperf) as zipf:
+                    files = zipf.namelist()
+                    assert len(files) == 1
+                    with zipf.open(files[0]) as inner:
+                        while chunk := inner.read(2**20):
+                            sqlf.write(chunk)
+
+            sqlf.flush()
+
+            async with aiosqlite.connect(sqlf.name) as sql:
+                sql.row_factory = aiosqlite.Row
+                yield sql
+
+    @contextlib.asynccontextmanager
+    async def get_gear_database(self):
+        for db in self._data['mobileGearAssetDataBases']:
+            if db['version'] == 2:
+                fragment = db['path']
+                break
+        else:
+            raise RuntimeError("WTF Bungie")
+
+        async with self._get_sqlite(path(fragment)) as sql:
+            yield sql
 
 
 async def get_manifest(client) -> Manifest:
